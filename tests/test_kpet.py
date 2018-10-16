57929c20d2ac690b5856bb7b7c7e86a47788ed55
Agustin Henze <tin@redhat.com>
2018-10-16 14:58:43 +0200

    Basic test cases for parsing arguments

--- tests/test_kpet.py
+++ tests/test_kpet.py
@@ -1,6 +1,39 @@
 import unittest
+import argparse
+import kpet
 
 
-class SkeletonTest(unittest.TestCase):
-    def test_dummy(self):
-        """Please fill me doing TDD as much as you can"""
+class ArgumentParserTest(unittest.TestCase):
+    def test_build_tree_command(self):
+        parser = argparse.ArgumentParser()
+        common_parser = argparse.ArgumentParser(add_help=False)
+        cmd_parser = parser.add_subparsers(dest="command")
+        kpet.build_tree_command(cmd_parser, common_parser)
+        args = parser.parse_args(['tree', 'list'])
+        self.assertEqual('tree', args.command)
+        self.assertEqual('list', args.action)
+
+    def test_build_arch_command(self):
+        parser = argparse.ArgumentParser()
+        common_parser = argparse.ArgumentParser(add_help=False)
+        cmd_parser = parser.add_subparsers(dest="command")
+        kpet.build_arch_command(cmd_parser, common_parser)
+        args = parser.parse_args(['arch', 'list'])
+        self.assertEqual('arch', args.command)
+        self.assertEqual('list', args.action)
+
+    def test_build_run_command(self):
+        parser = argparse.ArgumentParser()
+        common_parser = argparse.ArgumentParser(add_help=False)
+        cmd_parser = parser.add_subparsers(dest="command")
+        kpet.build_run_command(cmd_parser, common_parser)
+        self.assertRaises(SystemExit, parser.parse_args, ['run', 'generate'])
+
+        args = parser.parse_args(
+            ['run', 'generate', '-t', 'foo', '-k', 'bar', 'mbox1', 'mbox2']
+        )
+        self.assertEqual('run', args.command)
+        self.assertEqual('generate', args.action)
+        self.assertEqual('foo', args.tree)
+        self.assertEqual('bar', args.kernel)
+        self.assertListEqual(['mbox1', 'mbox2'], args.mboxes)
