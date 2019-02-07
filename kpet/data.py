0027636503a4ef6bb18f2b81904e5dacae9efe54
Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>
2019-02-07 12:15:25 +0200

    Add draft data and schema modules

---
+++ kpet/data.py
@@ -0,0 +1,113 @@
+# Copyright (c) 2019 Red Hat, Inc. All rights reserved. This copyrighted
+# material is made available to anyone wishing to use, modify, copy, or
+# redistribute it subject to the terms and conditions of the GNU General Public
+# License v.2 or later.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+"""KPET data"""
+
+import os
+from kpet.schema import Invalid, Int, Struct, StrictStruct, \
+    List, Dict, String, Regex, ScopedYAMLFile, YAMLFile, Class
+
+# pylint: disable=raising-format-tuple,useless-object-inheritance
+
+
+class Object(object):   # pylint: disable=too-few-public-methods
+    """An abstract data object"""
+    def __init__(self, schema, data):
+        """
+        Initialize an abstract data object with a schema validating and
+        resolving a supplied data.
+
+        Args:
+            schema: The schema of the data, must recognize to a Struct.
+            data:   The object data to be validated against and resolved with
+                    the schema.
+        """
+        # Validate and resolve the data
+        try:
+            data = schema.resolve(data)
+        except Invalid:
+            raise Invalid("Invalid {} data", type(self).__name__)
+
+        # Recognize the schema
+        schema = schema.recognize()
+        assert isinstance(schema, Struct)
+        try:
+            schema.validate(data)
+        except Invalid as exc:
+            raise Exception("Resolved data is invalid:\n{}".format(exc))
+
+        # Assign members
+        for member_name in schema.required.keys():
+            setattr(self, member_name, data[member_name])
+        for member_name in schema.optional.keys():
+            if member_name in data:
+                setattr(self, member_name, data[member_name])
+
+
+class TestSuite(Object):    # pylint: disable=too-few-public-methods
+    """Test suite"""
+    def __init__(self, data):
+        super(TestSuite, self).__init__(
+            StrictStruct(
+                description=String(),
+                version=String(),
+                patterns=List(StrictStruct(pattern=Regex(),
+                                           testcase_name=String())),
+                cases=List(
+                    Struct(
+                        required=dict(
+                            name=String(),
+                            tasks=String()
+                        ),
+                        optional=dict(
+                            hostRequires=String(),
+                            partitions=String(),
+                            kickstart=String()
+                        )
+                    )
+                )
+            ),
+            data
+        )
+
+
+class Base(Object):     # pylint: disable=too-few-public-methods
+    """Database"""
+
+    @staticmethod
+    def is_dir_valid(dir_path):
+        """
+        Check if a directory is a valid database.
+
+        Args:
+            dir_path:   Path to the directory to check.
+
+        Returns:
+            True if the directory is a valid database directory,
+            False otherwise.
+        """
+        return os.path.isfile(dir_path + "/layout/layout.json")
+
+    def __init__(self, dir_path):
+        """
+        Initialize a database object.
+        """
+        super(Base, self).__init__(
+            ScopedYAMLFile(
+                StrictStruct(
+                    schema=StrictStruct(version=Int()),
+                    testsuites=Dict(YAMLFile(Class(TestSuite)))
+                )
+            ),
+            dir_path + "/layout/layout.json")
+        self.dir_path = dir_path
---
+++ kpet/schema.py
@@ -0,0 +1,375 @@
+# Copyright (c) 2019 Red Hat, Inc. All rights reserved. This copyrighted
+# material is made available to anyone wishing to use, modify, copy, or
+# redistribute it subject to the terms and conditions of the GNU General Public
+# License v.2 or later.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+"""Database schema"""
+
+import re
+import os
+import yaml
+
+# pylint: disable=raising-format-tuple,useless-object-inheritance
+
+
+# The type returned by re.compile(). Different between Python 2 and 3
+_RE = type(re.compile(""))
+
+
+def _get_re_error_type():
+    """
+    Get the type of the exception produced when an invalid regular expression
+    is compiled.
+
+    Returns:
+        The "invalid regular expression" exception type.
+    Raises:
+        Exception   when the type cannot be discovered.
+    """
+    try:
+        re.compile("(")
+    except Exception as exc:    # pylint: disable=broad-except
+        return type(exc)
+    raise Exception("\"Invalid regex\" exception type not found")
+
+
+# The exception type produced by re.compile() on invalid regex.
+# Different between Python 2 and 3, and unavailable directly in Python 2
+_ReError = _get_re_error_type()
+
+
+class Invalid(Exception):
+    """Invalid data exception"""
+
+    def __init__(self, fmt, *args):
+        super(Invalid, self).__init__(fmt.format(*args))
+
+    def __str__(self):
+        # pylint: disable=no-member
+        return super(Invalid, self).__str__() + \
+               (":\n" + str(self.__context__)
+                if hasattr(self, "__context__") and self.__context__ else "")
+
+
+class Node(object):
+    """
+    Abstract node schema validating the data to be an instance of specified
+    type and resolving to the same.
+    """
+    def __init__(self, data_type):
+        """
+        Initialize a node schema.
+
+        Args:
+            data_type:  The type the data should be instance of.
+        """
+        self.data_type = data_type
+
+    def validate(self, data):
+        """
+        Validate data according to the schema.
+
+        Args:
+            The data to validate.
+
+        Raises:
+            Invalid:    The data didn't match the schema.
+        """
+        if not isinstance(data, self.data_type):
+            raise Invalid("Invalid type: {}, expecting {}",
+                          type(data).__name__, self.data_type.__name__)
+
+    def recognize(self):
+        """
+        Recognize the schema - return the schema that resolved data would
+        have.
+
+        Returns:
+            The schema the resolved data would have.
+        """
+        return self
+
+    def resolve(self, data):
+        """
+        Resolve (validate and massage) data according to the schema.
+
+        Args:
+            data:   The data to resolve.
+
+        Returns:
+            The resolved data. Will match the recognized schema.
+        """
+        self.validate(data)
+        return data
+
+
+class String(Node):
+    """String schema"""
+    def __init__(self):
+        super(String, self).__init__(str)
+
+
+class Int(Node):
+    """Integer number schema"""
+    def __init__(self):
+        super(Int, self).__init__(int)
+
+
+class Float(Node):
+    """Floating-point number schema"""
+    def __init__(self):
+        super(Float, self).__init__(float)
+
+
+class Boolean(Node):
+    """Boolean schema"""
+    def __init__(self):
+        super(Boolean, self).__init__(bool)
+
+
+class Regex(String):
+    """
+    Regular expression string schema.
+    """
+    def validate(self, data):
+        super(Regex, self).validate(data)
+        try:
+            re.compile(data)
+        except _ReError:
+            raise Invalid("Invalid regular expression")
+
+    def recognize(self):
+        return Node(_RE)
+
+    def resolve(self, data):
+        self.validate(data)
+        return re.compile(data)
+
+
+class RelativeFilePath(String):
+    """
+    Relative file path schema, resolved to the same schema and absolute file
+    path.
+    """
+    def resolve(self, data):
+        self.validate(data)
+        return os.path.realpath(data)
+
+
+class YAMLFile(String):
+    """
+    YAML file path schema, resolved to the file contents according to
+    specified schema.
+    """
+    def __init__(self, contents_schema):
+        assert isinstance(contents_schema, Node)
+        super(YAMLFile, self).__init__()
+        self.contents_schema = contents_schema
+
+    def recognize(self):
+        return self.contents_schema.recognize()
+
+    def resolve(self, data):
+        self.validate(data)
+        file_path = os.path.realpath(data)
+
+        # Load the data
+        with open(file_path, "r") as resolved_data_file:
+            resolved_data = yaml.load(resolved_data_file)
+
+        # Resolve loaded data
+        try:
+            return self.contents_schema.resolve(resolved_data)
+        except Invalid:
+            raise Invalid("Invalid contents of {}", file_path)
+
+
+class ScopedYAMLFile(YAMLFile):
+    """
+    YAML file path schema, resolved to file contents according to specified
+    schema, changes the current directory to the file's directory when
+    resolving the contents.
+    """
+    def resolve(self, data):
+        self.validate(data)
+        file_path = os.path.realpath(data)
+        dir_path = os.path.dirname(file_path)
+
+        # Load the data
+        with open(file_path, "r") as resolved_data_file:
+            resolved_data = yaml.load(resolved_data_file)
+
+        # Validate and resolve loaded data
+        orig_dir_path = os.getcwd()
+        os.chdir(dir_path)
+        try:
+            return self.contents_schema.resolve(resolved_data)
+        except Invalid:
+            raise Invalid("Invalid contents of {}", file_path)
+        finally:
+            os.chdir(orig_dir_path)
+
+
+class List(Node):
+    """
+    List schema, with every element matching a single specified schema.
+    """
+    def __init__(self, element_schema):
+        assert isinstance(element_schema, Node)
+        super(List, self).__init__(list)
+        self.element_schema = element_schema
+
+    def validate(self, data):
+        super(List, self).validate(data)
+        for index, value in enumerate(data):
+            try:
+                self.element_schema.validate(value)
+            except Invalid:
+                raise Invalid("Invalid value at index {}", index)
+
+    def recognize(self):
+        return List(self.element_schema.recognize())
+
+    def resolve(self, data):
+        self.validate(data)
+        return [self.element_schema.resolve(value) for value in data]
+
+
+class Dict(Node):
+    """
+    Dictionary schema, with string keys and every value matching a single
+    specified schema.
+    """
+    def __init__(self, value_schema):
+        assert isinstance(value_schema, Node)
+        super(Dict, self).__init__(dict)
+        self.value_schema = value_schema
+
+    def validate(self, data):
+        super(Dict, self).validate(data)
+        for key, value in data.items():
+            if not isinstance(key, str):
+                raise Invalid("Key \"{}\" is {}, expecting a string",
+                              key, type(key).__name__)
+            try:
+                self.value_schema.validate(value)
+            except Invalid:
+                raise Invalid("Invalid value with key \"{}\"", key)
+
+    def recognize(self):
+        return Dict(self.value_schema.recognize())
+
+    def resolve(self, data):
+        self.validate(data)
+        resolved_data = {}
+        for key, value in data.items():
+            resolved_data[key] = self.value_schema.resolve(value)
+        return resolved_data
+
+
+class Struct(Dict):
+    """
+    Dictionary schema, with string keys and each key having values with its
+    own schema.
+    """
+    def __init__(self, required=None, optional=None):
+        """
+        Initialize a struct schema.
+
+        Args:
+            required:   A dictionary of keys required to be present in the
+                        dictionary, mapped to their value schemas.
+            optional:   A dictionary of keys that can be present in the
+                        dictionary, but are not required, mapped to their
+                        value schemas.
+        """
+        assert required is None or isinstance(required, dict)
+        assert optional is None or isinstance(optional, dict)
+        super(Struct, self).__init__(Node(object))
+        if required is None:
+            required = {}
+        if optional is None:
+            optional = {}
+        for key, value in required.items():
+            assert isinstance(key, str)
+            assert isinstance(value, Node)
+        for key, value in optional.items():
+            assert isinstance(key, str)
+            assert isinstance(value, Node)
+        self.required = required
+        self.optional = optional
+
+    def validate(self, data):
+        super(Struct, self).validate(data)
+        for name, schema in self.required.items():
+            if name not in data:
+                raise Invalid("Member \"{}\" is missing", name)
+            value = data[name]
+            try:
+                schema.validate(value)
+            except Invalid:
+                raise Invalid("Member \"{}\" is invalid", name)
+        for name, schema in self.optional.items():
+            if name in data:
+                value = data[name]
+                try:
+                    schema.validate(value)
+                except Invalid:
+                    raise Invalid("Member \"{}\" is invalid", name)
+
+    def recognize(self):
+        recognized_required = {}
+        recognized_optional = {}
+
+        for name, schema in self.required.items():
+            recognized_required[name] = schema.recognize()
+        for name, schema in self.optional.items():
+            recognized_optional[name] = schema.recognize()
+
+        return Struct(required=recognized_required,
+                      optional=recognized_optional)
+
+    def resolve(self, data):
+        self.validate(data)
+        resolved_data = {}
+
+        for name, schema in self.required.items():
+            resolved_data[name] = schema.resolve(data[name])
+        for name, schema in self.optional.items():
+            if name in data:
+                resolved_data[name] = schema.resolve(data[name])
+
+        return resolved_data
+
+
+class StrictStruct(Struct):
+    """
+    Struct schema with required keys only.
+    """
+    def __init__(self, **kwargs):
+        super(StrictStruct, self).__init__(required=kwargs)
+
+
+class Class(Node):
+    """
+    Class instance schema, resolves to a class instance with (arbitrary) data
+    as the creation argument.
+    """
+    def __init__(self, instance_type):
+        super(Class, self).__init__(object)
+        self.instance_type = instance_type
+
+    def recognize(self):
+        return Node(object)
+
+    def resolve(self, data):
+        self.validate(data)
+        return self.instance_type(data)
