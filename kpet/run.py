f02685df304ac92e084027e07c02248e3b505a6e
Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>
2019-02-11 13:39:14 +0200

    run: Pass database to generate()

--- kpet/run.py
+++ kpet/run.py
@@ -51,7 +51,7 @@ def print_test_cases(patches, dbdir, pw_cookie=None):
 
 
 # pylint: disable=too-many-arguments
-def generate(template, template_params, patches, dbdir, output,
+def generate(template, template_params, patches, database, output,
              pw_cookie=None):
     """
     Generate an xml output compatible with beaker.
@@ -60,23 +60,24 @@ def generate(template, template_params, patches, dbdir, output,
         template_params: A dictionary with template parameters such as arch,
                          whiteboard description, etc.
         patches:         List of patches, can be local files or remote urls
-        dbdir:           Path to the kpet-db
+        database:        Database instance
         output:          Output file where beaker xml will be rendered
         pw_cookie:       Session cookie to Patchwork instance if login is
                          required, None otherwise
     """
-    test_names = get_test_cases(patches, dbdir, pw_cookie)
+    test_names = get_test_cases(patches, database.dir_path, pw_cookie)
     template_params['TEST_CASES'] = sorted(
-        targeted.get_property('tasks', test_names, dbdir, required=True)
+        targeted.get_property('tasks', test_names, database.dir_path,
+                              required=True)
     )
     template_params['TEST_CASES_HOST_REQUIRES'] = sorted(
-        targeted.get_property('hostRequires', test_names, dbdir)
+        targeted.get_property('hostRequires', test_names, database.dir_path)
     )
     template_params['TEST_CASES_PARTITIONS'] = sorted(
-        targeted.get_property('partitions', test_names, dbdir)
+        targeted.get_property('partitions', test_names, database.dir_path)
     )
     template_params['TEST_CASES_KICKSTART'] = sorted(
-        targeted.get_property('kickstart', test_names, dbdir)
+        targeted.get_property('kickstart', test_names, database.dir_path)
     )
     content = template.render(template_params)
     if not output:
@@ -90,6 +91,7 @@ def main(args):
     """Main function for the `run` command"""
     if not data.Base.is_dir_valid(args.db):
         raise Exception("\"{}\" is not a database directory".format(args.db))
+    database = data.Base(args.db)
     if args.action == 'generate':
         template = utils.get_jinja_template(args.tree, args.db)
         template_params = {
@@ -99,8 +101,8 @@ def main(args):
             'TREE': args.tree,
             'getenv': os.getenv,
         }
-        generate(template, template_params, args.mboxes, args.db, args.output,
-                 args.pw_cookie)
+        generate(template, template_params, args.mboxes, database,
+                 args.output, args.pw_cookie)
     elif args.action == 'print-test-cases':
         print_test_cases(args.patches, args.db, args.pw_cookie)
     else:
