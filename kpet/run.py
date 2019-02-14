3edcab00c5605893bc84344c5dbe8e372fa001f6
Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>
2019-02-14 19:14:53 +0200

    Do not import print function from future

--- kpet/run.py
+++ kpet/run.py
@@ -12,7 +12,6 @@
 # this program; if not, write to the Free Software Foundation, Inc., 51
 # Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 """Module where the `run` command is implemented"""
-from __future__ import print_function
 import tempfile
 import shutil
 import os
