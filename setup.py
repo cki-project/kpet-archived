1d3ee178db54992906d94bfee0bf966a57c932b9
Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>
2019-02-18 14:03:44 +0200

    Check Python version in setup.py
    Switch from using "python_requires" in python.cfg to explicit code in
    setup.py to support older versions of pip (such as one found on
    CentOS 7), which would otherwise ignore the requirement.

--- setup.py
+++ setup.py
@@ -13,6 +13,18 @@
 # along with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 """Install kpet using setuptools."""
+import sys
 from setuptools import setup
+
+python_required_major = 3
+python_required_minor = 6
+
+if sys.version_info.major < python_required_major or \
+   sys.version_info.major == python_required_major and \
+   sys.version_info.minor < python_required_minor:
+    sys.stderr.write("Python v{}.{} is required, but running v{}.{}".
+                     format(python_required_major, python_required_minor,
+                            sys.version_info.major, sys.version_info.minor))
+    sys.exit(1)
 
 setup()
