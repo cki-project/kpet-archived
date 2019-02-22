f6a670eb2de33b8652841697c97366aff7d8bcea
Jakub Racek <jracek@redhat.com>
2019-02-22 14:41:51 +0100

    increase run module coverage
    Signed-off-by: Jakub Racek <jracek@redhat.com>

--- tests/test_run.py
+++ tests/test_run.py
@@ -15,12 +15,19 @@
 import os
 import tempfile
 import unittest
+from io import StringIO
+
 import mock
+
 from kpet import run, data, exceptions
 
 
 class RunTest(unittest.TestCase):
     """Test cases for run module."""
+
+    def setUp(self):
+        self.dbdir = os.path.join(os.path.dirname(__file__), 'assets')
+
     def test_generate(self):
         """
         Check the success case, if it raises the proper exception when
@@ -32,13 +39,12 @@ class RunTest(unittest.TestCase):
             'KURL': 'bar',
             'ARCH': 'baz',
         }
-        dbdir = os.path.join(os.path.dirname(__file__), 'assets')
-        database = data.Base(dbdir)
+        database = data.Base(self.dbdir)
         template = database.get_tree_template('rhel7')
         with mock.patch('sys.stdout') as mock_stdout:
             run.generate(template, template_params, [], database, True, None)
-        with open(os.path.join(dbdir, 'rhel7_rendered.xml')) as file_handler:
-            content_expected = file_handler.read()
+        with open(os.path.join(self.dbdir, 'rhel7_rendered.xml')) as fhandle:
+            content_expected = fhandle.read()
         self.assertEqual(
             mock.call(content_expected),
             mock_stdout.write.call_args_list[0],
@@ -91,3 +97,83 @@ class RunTest(unittest.TestCase):
 
         mock_args.action = 'action-not-found'
         self.assertRaises(exceptions.ActionNotFound, run.main, mock_args)
+
+    def test_invalid_dbdir(self):
+        """Check invalid dbdir raises an exception."""
+        mock_args = mock.Mock()
+        mock_args.action = 'generate'
+        mock_args.tree = 'rhel7'
+        mock_args.kernel = 'kernel'
+        mock_args.arch = 'arch'
+        mock_args.db = '/tmp/igotgoosebutnoshoes'
+        mock_args.output = None
+        mock_args.pw_cookie = None
+        mock_args.description = 'description'
+        mock_args.mboxes = []
+
+        with self.assertRaises(Exception):
+            run.main(mock_args)
+
+    @mock.patch('sys.stdout', new_callable=StringIO)
+    def test_print_testcases(self, mock_stdout):
+        """Check invalid dbdir raises an exception."""
+        mock_args = mock.Mock()
+        mock_args.action = 'print-test-cases'
+        mock_args.tree = 'rhel7'
+        mock_args.kernel = 'kernel'
+        mock_args.arch = 'arch'
+        mock_args.db = os.path.join(os.path.dirname(__file__), 'assets')
+        mock_args.output = None
+        mock_args.pw_cookie = None
+        mock_args.description = 'description'
+        mock_args.mboxes = []
+        mock_args.patches = []
+
+        run.main(mock_args)
+
+        expected = ["default/ltplite", "fs/ext4", "fs/xfs"]
+
+        self.assertEqual(mock_stdout.getvalue().splitlines(), expected)
+
+    def test_invalid_tree(self):
+        """Check invalid tree raises an exception."""
+
+        mock_args = mock.Mock()
+        mock_args.action = 'generate'
+        mock_args.tree = 'rhel0'
+        mock_args.kernel = 'kernel'
+        mock_args.arch = 'arch'
+        mock_args.db = os.path.join(os.path.dirname(__file__), 'assets')
+        mock_args.output = None
+        mock_args.pw_cookie = None
+        mock_args.description = 'description'
+        mock_args.mboxes = []
+
+        with self.assertRaises(Exception):
+            run.main(mock_args)
+
+    def _assert_standard_testcases(self, mock_stdout):
+
+        self.assertEqual(
+            mock_stdout.write.call_args_list[0],
+            mock.call('default/ltplite'),
+        )
+        self.assertEqual(
+            mock_stdout.write.call_args_list[2],
+            mock.call('fs/ext4'),
+        )
+        self.assertEqual(
+            mock_stdout.write.call_args_list[4],
+            mock.call('fs/xfs'),
+        )
+
+    @mock.patch('sys.stdout', new_callable=StringIO)
+    def test_get_test_cases_print(self, mock_stdout):
+        """Check method prints output as it should."""
+        database = data.Base(self.dbdir)
+
+        run.print_test_cases([], database)
+
+        expected = ["default/ltplite", "fs/ext4", "fs/xfs"]
+
+        self.assertEqual(mock_stdout.getvalue().splitlines(), expected)
