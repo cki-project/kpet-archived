58d4d3e94f06300453a65b33b6f9f6b5ac5c65bf
Jakub Racek <jracek@redhat.com>
2019-06-17 15:53:01 +0200

    remove tasks from host type
    This implements FASTMOVING-826.
    
    Adding tasks through host type is no longer used, so let's remove it.
    
    Signed-off-by: Jakub Racek <jracek@redhat.com>

--- kpet/run.py
+++ kpet/run.py
@@ -69,7 +69,6 @@ class Host:
         self.hostRequires = type.hostRequires
         self.partitions = type.partitions
         self.kickstart = type.kickstart
-        self.tasks = type.tasks
         self.suites = suites
 
         self.name = name
